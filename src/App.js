d19b9a4d491da85465006297e1ee1f51a5100245
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-25 08:40:53 +0100

    Finishing firebase setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: 'AIzaSyABadeye19JbLQe0uKQFjtssM6vvEYpWQU',
+      authDomain: 'fluted-magpie-102108.firebaseapp.com',
+      databaseURL: 'https://fluted-magpie-102108.firebaseio.com',
+      projectId: 'fluted-magpie-102108',
+      storageBucket: 'fluted-magpie-102108.appspot.com',
+      messagingSenderId: '988113929255'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
